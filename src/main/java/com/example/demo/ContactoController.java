926bfa59a1e58a3e4da60c3b5c03cf987a92742d
cristychavajay8-hue <cristychavajay8@gmail.com>
2026-08-16 16:26:46 -0600

    De nuevo

--- src/main/java/com/example/demo/ContactoController.java
+++ src/main/java/com/example/demo/ContactoController.java
@@ -13,24 +13,11 @@ public class ContactoController {
     @Autowired
     private com.example.demo.service.EmailService emailService;
 
- @PostMapping("/contacto")
-public ResponseEntity<String> recibirContacto(@RequestBody Map<String, String> datos) {
-    System.out.println("Formulario recibido con éxito.");
+@PostMapping("/contacto")
+    public ResponseEntity<String> recibirContacto(@RequestBody Map<String, String> datos) {
+        System.out.println("Formulario recibido con éxito.");}
 
-    String correo = datos.get("correo");
-    String telefono = datos.get("telefono");
-    String empresa = datos.get("empresa");
-    String asunto = datos.get("asunto");
-    String mensaje = datos.get("mensaje");
+        emailService.enviarCorreoContacto(datos);
 
-    String contenido = "Nuevo mensaje de contacto:\n\n" +
-            "Correo: " + correo + "\n" +
-            "Teléfono: " + telefono + "\n" +
-            "Empresa: " + empresa + "\n" +
-            "Asunto: " + asunto + "\n" +
-            "Mensaje: " + mensaje;
-
-    emailService.enviarCorreo("cristychavajay8@gmail.com", "Nuevo mensaje: " + asunto, contenido);
-
-    return ResponseEntity.ok("Formulario enviado con éxito");
-}
+        return ResponseEntity.ok("¡Mensaje recibido correctamente en Spring Boot!");
+    }
