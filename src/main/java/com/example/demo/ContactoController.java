ea704e84609395d1a399e14749407c86f23dc707
cristychavajay8-hue <cristychavajay8@gmail.com>
2026-08-16 21:18:39 -0600

    cdskhbv

--- src/main/java/com/example/demo/ContactoController.java
+++ src/main/java/com/example/demo/ContactoController.java
@@ -5,19 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.service.EmailService;
 import java.util.Map;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.POST, RequestMethod.OPTIONS})
+@CrossOrigin(origins = "*")
 public class ContactoController {
 
     @Autowired
     private EmailService emailService;
 
-    @PostMapping("/contacto")
+    @RequestMapping(value = "/contacto", method = {RequestMethod.POST, RequestMethod.OPTIONS})
     public ResponseEntity<String> recibirContacto(@RequestBody Map<String, String> datos) {
-       
         emailService.enviarCorreoContacto(datos);
-        return ResponseEntity.ok("¡Mensaje recibido correctamente!");
+        return ResponseEntity.ok("Mensaje recibido correctamente");
     }
 }
